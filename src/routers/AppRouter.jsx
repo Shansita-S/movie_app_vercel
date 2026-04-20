841093a7adff9c40c0fe92bd11a305d1b5ae4ab8
Shansita S <shansita@example.com>
2026-04-20 10:13:25 +0530

    fix: restore browser routing with Vercel rewrites

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
